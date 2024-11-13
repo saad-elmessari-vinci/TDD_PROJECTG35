00f154387c228223644faba950956e7393fa58e1
elmessari <saad.elmessari@student.vinci.be>
2024-11-13 13:03:23 +0100

    add: when given 1 minute, return YOOO

--- spec/berlinClockSpec.js
+++ spec/berlinClockSpec.js
@@ -4,10 +4,18 @@ describe("SingleMinutesRow function should return the answer ", function () {
   
   it("Should return 'OOOO' for 0 minute", function () {
     const main = new Main();
-    
+
     const result = main.singleMinutesRow(0);
 
     expect(result).toBe("OOOO");
+  });
+
+  it("Should return 'YOOO' for 1 minute", function () {
+    const main=new Main();
+
+    const result = main.singleMinutesRow(1);
+
+    expect(result).toBe("YOOO");
   });
 
 });
--- src/berlinClock.js
+++ src/berlinClock.js
@@ -1,6 +1,8 @@
 export class Main {
   //Y ==> lamp is on (color is yellow) ,R==> lamp is on (color is red), O==> lamp is off
   singleMinutesRow(minutes) {
+    if(minutes===1) return 'YOOO';
+    
     return "OOOO";
   }
 }
